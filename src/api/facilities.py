6a8bd55f70b68d3a3b02607ac328b07cf22dbb16
pursvir <i-have@no.email>
2025-06-09 14:37:39 +0300

    Task 15: GET and POST facilities

---
+++ src/api/facilities.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from src.schemas.bookings import BookingAddRequest, BookingAdd
+from src.api.dependencies import DBDep, UserIDDep
+from src.schemas.facilities import Facility, FacilityAdd
+
+
+router = APIRouter(prefix="/facilities", tags=["Удобства"])
+
+@router.get("")
+async def get_facilities(db: DBDep):
+    return await db.facilities.get_all()
+
+@router.post("")
+async def add_facility(db: DBDep, data: FacilityAdd):
+    new_facility_data = await db.facilities.add(data)
+    await db.commit()
+    return {"status": "OK", "data": new_facility_data}
--- src/utils/db_manager.py
+++ src/utils/db_manager.py
@@ -1,4 +1,5 @@
 
+from src.repos.facilities import FacilitiesRepository
 from src.repos.hotels import HotelsRepository
 from src.repos.rooms import RoomsRepository
 from src.repos.users import UsersRepository
@@ -15,6 +16,7 @@ class DBManager():
         self.rooms = RoomsRepository(self.session)
         self.users = UsersRepository(self.session)
         self.bookings = BookingsRepository(self.session)
+        self.facilities = FacilitiesRepository(self.session)
 
         return self
 
